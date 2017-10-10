88ec9bba2f47ace33438e931cd5d89e6e66e9dd1
Maximilian Stoiber <contact@mxstbr.com>
2017-10-10 19:29:58 +0200

    Forward pathname, search and hash
    Going to `micro-redirect-instance.com/test?a=b#c` will redirect to
    `REDIRECT_URL/test?a=b#c`.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 if (!process.env.REDIRECT_URL) throw new Error('Please provide the REDIRECT_URL environment variable. See https://github.com/withspectrum/micro-redirect for more information.')
+const { URL } = require('url');
 
 module.exports = async (req, res) => {
   res.statusCode = 301;
-  res.setHeader('Location', process.env.REDIRECT_URL);
+  const url = new URL(req.url, process.env.REDIRECT_URL);
+  res.setHeader('Location', `${process.env.REDIRECT_URL}${url.pathname ? url.pathname : ''}${url.search ? url.search : ''}${url.hash ? url.hash : ''}`);
   res.end();
 }
